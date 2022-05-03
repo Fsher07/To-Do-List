ef13b18f02d2ea63804ac78a379cd13830768f59
Fsher07 <furkansalimm@gmail.com>
2022-05-03 13:08:35 +0300

    Update taskOperations.test.js

--- src/__test__/taskOperations.test.js
+++ src/__test__/taskOperations.test.js
@@ -1,9 +1,9 @@
-import TaskOperations from "../taskOperations";
+import TaskOperations from '../taskOperations.js';
 
-describe ("TaskOperations", () => {
-  test ('should add to localstorage', () => {
-    const Task = {index:1, description: "test", completed: false};
-      TaskOperations.addTask();
-      expect(JSON.parse(localStorage.getItem('tasksData'))).toEqual(Task);
+describe('TaskOperations', () => {
+  test('should add to localstorage', () => {
+    const Task = { index: 1, description: 'test', completed: false };
+    TaskOperations.addTask();
+    expect(JSON.parse(localStorage.getItem('tasksData'))).toEqual(Task);
   });
 });
